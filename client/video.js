3950db31abc46c57c22d4b25d0872723ee0592f2
AlexK <alexk@LoneSurvivor.blinkbox.local>
2015-05-21 17:55:30 +0100

    vote totals etc are working pretty nicely (pretty tricky to get the combination correct0

--- client/video.js
+++ client/video.js
@@ -16,23 +16,38 @@ var videosMergedWithVotes = function() {
     var votes = Votes.find().fetch();
 
     return _.map(videos, function(video, index) {
-      video.votes =  _.where(votes, { videoId: video._id }).length;
+      video.votes =  _.where(votes, { videoId: video._id });
+      video.voteCount = video.votes.length;
       return video;
     });
-}
+};
 
 //custom reactive data source
-var votesNeeded = new ReactiveVar(100);
+var votesNeeded = new ReactiveVar(1);
 
 //setup a reactive computation
 Tracker.autorun(function () {
-  votesNeeded.set(Math.floor(Presences.find().count() / 2) + 1);
+  var peopleOnline = Presences.find().count();
+  
+  var _votesNeeded = 1;
+  
+  if(peopleOnline === 2) {
+    _votesNeeded = 2;
+  } else if (peopleOnline > 2) {
+    //the majority rules
+    _votesNeeded = Math.floor(peopleOnline) + 1;
+  }
+  
+  votesNeeded.set(_votesNeeded);
 });
 
 
 Template.body.helpers({
   voteCount: function() {
     return Votes.find().count(); //reactive data source so is live
+  },
+  votesNeeded: function() {
+    return votesNeeded.get();
   },
   videos: function() {
     return videosMergedWithVotes();
@@ -52,30 +67,22 @@ Template.video.events({
   "click .vote": function (event, template) {
     //this runs a simulation to handle latency compensation.
     Meteor.call("vote", this._id);
-    
-    //You would of thought I'd get a value back here to update the progress bar - nope it's reactive baby?
-    
-    var hasVote = template.hasVote.get();
-    template.hasVote.set(!hasVote);
+
   }
 });
 
 
-var votingIsDone = function() {
-    var data = Template.currentData();
-    
-    return data.votes >= votesNeeded.get();
-}
 
 Template.video.helpers({
   votesNeeded: function() {
     return votesNeeded.get(); //note the tempalte does not have access to outer variables 
   },
-  votingIsDone: function() {
-    return votingIsDone();
-  },
   hasVote: function() {
-    return Template.instance().hasVote.get();
+    var videoVotes = Template.currentData().votes;
+    
+    //console.log("boom"); //if you did this, you'd see it's pretty chatty here
+    
+    return videoVotes.length && _.where(videoVotes, { owner : Meteor.userId() }) ? true : false;
   }
 });
 
@@ -87,14 +94,9 @@ Template.video.onRendered(function(){
   //make itself relient on it's dom changes
   self.autorun(function () {
     var data = Template.currentData(); //knows about the current instance
-    
+
     //suprisingly tricky to get the correct syntax!!
-    Template.instance().$(".progress").progress({value: data.votes, total: data.votesNeeded });
+    Template.instance().$(".progress").progress({value: data.voteCount, total: votesNeeded.get() });
   });
 });
 
-
-Template.video.onCreated(function() {
-  this.hasVote = new ReactiveVar;
-  this.hasVote.set(false);
-});
